9e8cec12b97306b7536cda29f0f098b6a0ef4b33
Antoine GIRARD <sapk@sapk.fr>
2019-03-17 22:36:59 +0100

    install postgres + lca

---
+++ script/lca.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#Usefull command to work with Genesys LCA
+
+#TODO check deps like lca ^^
+
+function Genesys_Start_LCA {
+  local port=${1:-'4999'}
+  
+  echo "Start-up: Starting LCA to launch on port $port..."
+  (cd /gcti/lca && echo "standard = stdout" >> lca.cfg && nohup /gcti/lca/lca "$port" &)
+}
